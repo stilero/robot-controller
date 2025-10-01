9bff86b3f4f985e4e6b9408b0d892efac73bbd61
Daniel Eliasson <daniel@stilero.com>
2025-10-01 21:31:23 +0200

    Refactor DirectionOperation methods and add WalkForward
    Improved TurnLeft and TurnRight methods by introducing a
    private static readonly variable MaxDirectionValue to
    store the maximum value of the Direction enum, enhancing
    code readability and maintainability. Added WalkForward
    method to provide movement coordinates based on the
    current direction.

--- src/RobotControllerApp/Domain/Direction.cs
+++ src/RobotControllerApp/Domain/Direction.cs
@@ -10,8 +10,18 @@ public enum Direction
 
 public static class DirectionOperation
 {
-    public static Direction TurnLeft(this Direction currentDirection) => (Direction)((int)currentDirection -1 < 0 ? (int)Enum.GetValues<Direction>().Max() : (int)currentDirection - 1);
-    public static Direction TurnRight(this Direction currentDirection) => (Direction)((int)currentDirection + 1 > (int)Enum.GetValues<Direction>().Max() ? 0 : (int)currentDirection + 1);
+    private static readonly int MaxDirectionValue = (int)Enum.GetValues<Direction>().Max();
+
+    public static Direction TurnLeft(this Direction currentDirection) => 
+        (Direction)((int)currentDirection -1 < 0 
+        ? MaxDirectionValue
+        : (int)currentDirection - 1);
+
+    public static Direction TurnRight(this Direction currentDirection) => 
+        (Direction)((int)currentDirection + 1 > MaxDirectionValue
+        ? 0 
+        : (int)currentDirection + 1);
+
     public static (int movementX, int movementY) WalkForward(this Direction currentDirection) => currentDirection switch
     {
         Direction.North => (0, -1),
