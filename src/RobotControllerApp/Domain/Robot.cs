48836aec34c1960321abbe3cfcdc772c7c8d135b
Daniel Eliasson <daniel@stilero.com>
2025-10-01 21:55:39 +0200

    Refactor Robot movement methods to use Movement class
    Updated the `TurnLeft`, `TurnRight`, and `WalkForward` methods in the `Robot` class to utilize the `Movement` class instead of `DirectionOperation`. This change improves the handling of the robot's directional movements.

--- src/RobotControllerApp/Domain/Robot.cs
+++ src/RobotControllerApp/Domain/Robot.cs
@@ -3,12 +3,12 @@
 public sealed class Robot(int x, int y, Direction direction)
 {
 
-    public void TurnLeft() => direction = DirectionOperation.TurnLeft(direction);
-    public void TurnRight() => direction = DirectionOperation.TurnRight(direction);
+    public void TurnLeft() => direction = Movement.TurnLeft(direction);
+    public void TurnRight() => direction = Movement.TurnRight(direction);
    
     public void WalkForward()
     {
-        var (movementX, movementY) = DirectionOperation.WalkForward(direction);
+        var (movementX, movementY) = Movement.WalkForward(direction);
         x += movementX;
         y += movementY;
     }
