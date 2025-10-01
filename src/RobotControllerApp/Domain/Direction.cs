22c0d9abb44bd32b9165fe84732bc7dc771f112a
Daniel Eliasson <daniel@stilero.com>
2025-10-01 20:52:32 +0200

    Add Direction enum and rotation methods with tests
    Implemented a new `Direction` enum for cardinal directions and added a `DirectionOperation` class with `TurnLeft` and `TurnRight` methods. Created unit tests in `DirectionTests` to validate the functionality of these methods.

---
+++ src/RobotControllerApp/Domain/Direction.cs
@@ -0,0 +1,16 @@
+﻿namespace RobotControllerApp.Domain;
+
+public enum Direction
+{
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3
+}
+
+public static class DirectionOperation
+{
+    public static Direction TurnLeft(this Direction currentDirection) => (Direction)((int)currentDirection -1 < 0 ? (int)Enum.GetValues<Direction>().Max() : (int)currentDirection - 1);
+    public static Direction TurnRight(this Direction currentDirection) => (Direction)((int)currentDirection + 1 > (int)Enum.GetValues<Direction>().Max() ? 0 : (int)currentDirection + 1);
+
+}
---
+++ tests/RobotControllerAppTests/DirectionTests.cs
@@ -0,0 +1,32 @@
+﻿using RobotControllerApp.Domain;
+
+namespace RobotControllerAppTests;
+
+public sealed class DirectionTests
+{
+    [Theory]
+    [InlineData(Direction.North, Direction.West)]
+    [InlineData(Direction.West, Direction.South)]
+    [InlineData(Direction.South, Direction.East)]
+    [InlineData(Direction.East, Direction.North)]
+    public void TurnLeft_ShouldReturnExpectedDirection(Direction current, Direction expected)
+    {
+        //Act
+        var actual = DirectionOperation.TurnLeft(current);
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(Direction.North, Direction.East)]
+    [InlineData(Direction.East, Direction.South)]
+    [InlineData(Direction.South, Direction.West)]
+    [InlineData(Direction.West, Direction.North)]
+    public void TurnRight_ShouldReturnExpectedDirection(Direction current, Direction expected)
+    {
+        //Act
+        var actual = DirectionOperation.TurnRight(current);
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+}
